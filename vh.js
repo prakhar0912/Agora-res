4719ea9b26ea8fd832fa2bb8f8dc8d732931a404
Prakhar <prakhar@pop-os.localdomain>
2020-12-16 12:44:18 +0530

    mobile resize testing #4

--- vh.js
+++ vh.js
@@ -1,4 +1,8 @@
-if (window.innerWidth < 800) {
-    nice = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${nice}px`);
+let resiz = () => {
+    if (window.innerWidth < 800) {
+        nice = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', `${nice}px`);
+    }
 }
+resiz()
+window.addEventListener('resize', resiz)
